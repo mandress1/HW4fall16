8e30277fdfda51db12751e0a4145f1a452b41cc0
Matt Andress <matthew-andress@uiowa.edu>
2016-09-23 22:43:18 +0000

    got rid of accident migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150809022253) do
+ActiveRecord::Schema.define(version: 20160923222607) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -20,6 +20,9 @@ ActiveRecord::Schema.define(version: 20150809022253) do
     t.datetime "release_date"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
   end
 
 end
