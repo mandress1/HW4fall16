b11fbacc6d05616e238da83fe2de2829ccb8e57f
Matt Andress <matthew-andress@uiowa.edu>
2016-09-25 20:39:12 +0000

    hopefully started create methd correctly

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,15 +1,17 @@
 class UsersController < ApplicationController
     
     def user_params
-       #params.require 
+       params.require(:user).permit(:id, :email) 
     end
     
     def new
-      
+      # default new template
     end
     
     def create
-      
+      @user = User.create!(user_params)
+      flash[:notice] = "#{@user.id} was created"
+      redirect_to.movies_path
     end
 
 end
