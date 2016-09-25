e88f7f9e60e08be0bc4c7ccc46bff91d1f8e57cd
Matt Andress <matthew-andress@uiowa.edu>
2016-09-25 21:38:26 +0000

    think i added an incomplete create method. pls work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,16 @@
 class UsersController < ApplicationController
+  
+  def user_params
+    params.require(:user).permit(:user_id, :email)  
+  end
+  
   def new
   end
+  
+  def create
+    @user = User.create!(user_params)
+    flash[:notice] = "#{@user.user_id} was created"
+    redirect_to movie_path
+  end
+  
 end
