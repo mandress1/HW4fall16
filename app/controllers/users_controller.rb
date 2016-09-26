e5d43f562389faa3872e7c3624bcacb3f72e4e73
Matt Andress <matthew-andress@uiowa.edu>
2016-09-26 05:08:18 +0000

    now redirecting to root instead of users list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
   end
   
   def create
+    # do i need to make sure the email is in the correct format?
     new_params = user_params
     if new_params[:user_id].empty? or new_params[:email].empty?
       flash[:warning] = "All fields must be filled out"
@@ -17,7 +18,7 @@ class UsersController < ApplicationController
       if User.find_by(user_id: new_params[:user_id]).nil?
         @user = User.create_user!(new_params)
         flash[:notice] = "Welcome #{@user.user_id}. Your accout has been created"
-        redirect_to users_path
+        redirect_to root_path
       else
         flash[:notice] = "Sorry, that user-id already exists"
         redirect_to new_user_path
