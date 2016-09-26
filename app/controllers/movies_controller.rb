09d01518d9e5a574ac781fa3811ba2333f052d7a
Matt Andress <matthew-andress@uiowa.edu>
2016-09-26 13:45:29 +0000

    now setting current user now with before filter

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,7 @@
 class MoviesController < ApplicationController
 
+  before_filter :set_current_user
+  
   def movie_params
     params.require(:movie).permit(:title, :rating, :description, :release_date)
   end
@@ -40,6 +42,10 @@ class MoviesController < ApplicationController
     @movie.destroy
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
+  end
+  
+  def set_current_user
+    @current_user ||= session[:session_token] && User.find_by(session_token: session[:session_token])
   end
 
 end
