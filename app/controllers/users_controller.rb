854bfecfd9285f284daac7a2bd600206e5b06a55
Matt Andress <matthew-andress@uiowa.edu>
2016-09-26 04:52:33 +0000

    now checking for valid user info and calling method from model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,20 @@ class UsersController < ApplicationController
   end
   
   def create
-    User.create_user!(user_params()[:user])
+    new_params = user_params
+    if new_params[:user_id].empty? or new_params[:email].empty?
+      flash[:warning] = "All fields must be filled out"
+      redirect_to new_user_path
+    else
+      if User.find_by(user_id: new_params[:user_id]).nil?
+        @user = User.create_user!(new_params)
+        flash[:notice] = "Welcome #{@user.user_id}. Your accout has been created"
+        redirect_to users_path
+      else
+        flash[:notice] = "Sorry, that user-id already exists"
+        redirect_to new_user_path
+      end
+    end
   end
   
   def index
