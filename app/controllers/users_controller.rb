f1c93570a8cf863699bf7c9de772b5f5f8d94c75
Matt Andress <matthew-andress@uiowa.edu>
2016-09-26 01:58:52 +0000

    now null checking

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,13 +9,18 @@ class UsersController < ApplicationController
   end
   
   def create
-    if User.find_by(user_id: params[:user][:user_id]) == nil
-      @user = User.create!(user_params)
-      flash[:notice] = "#{@user.user_id} was created"
-      redirect_to users_path
-    else
-      flash[:notice] = "Sorry, that user-id already exists"
+    if params[:user][:user_id].empty? or params[:user][:email].empty?
+      flash[:warning] = "All fields must be filled out"
       redirect_to new_user_path
+    else
+      if User.find_by(user_id: params[:user][:user_id]) == nil
+        @user = User.create!(user_params)
+        flash[:notice] = "#{@user.user_id} was created"
+        redirect_to users_path
+      else
+        flash[:notice] = "Sorry, that user-id already exists"
+        redirect_to new_user_path
+      end
     end
   end
   
