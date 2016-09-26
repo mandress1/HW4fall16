f648050e760a1c3d77fccf2d4cb1c5959f9d6186
Matt Andress <matthew-andress@uiowa.edu>
2016-09-26 11:34:50 +0000

    probably won't work, but good start on session thingy. at least loging in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,26 @@
 class SessionsController < ApplicationController
+  
+  def session_params
+    params.require(:session).permit(:user_id, :email)  
+  end
+  
+  def create
+    params = session_params
+    usr = User.find_by(user_id: params[:user_id])
+    
+    if not usr.nil?
+      if usr.email == params[:email]
+        session[:session_token] = usr.session_token
+        flash[:notice] = "You are logged in as #{usr.user_id}"
+        redirect_to root_path
+      else
+        flash[:warning] = "Invalid user-id/e-mail combination"
+        redirect_to login_path
+      end
+    else
+      flash[:warning] = "Invalid user-id/e-mail combination"
+      redirect_to login_path
+    end
+  end
+  
 end
